bf3042783d3ebddc76513e3eb066ce7e69e59111
Jakub Bielanski <gg18051805@gmail.com>
2022-10-25 08:12:00 +0200

    refactored script to eliminate repetition, added constants file

---
+++ projekt/templates/constants.php
@@ -0,0 +1,6 @@
+<?PHP
+namespace Projekt\Templates;
+
+const DIV='<div class="product"><input class ="delete-checkbox" type="checkbox" id="product" name="product';
+const VALUESTRING='" value="';
+const CLOSEP='"><p>';
